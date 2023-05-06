1ee02e730bc9031d5016a8d8a1b91758de8986b1
Vitalii Borshchov <vitalii.v.borshchov@gmail.com>
2023-05-06 15:23:16 +0300

    feat: decomposition of the AppRouter component

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -5,37 +5,19 @@ import { UserRoles } from '../../types/types';
 import PublicRoutes from './components/PublicRoutes/PublicRoutes';
 import PrivateAdminRoutes from './components/PrivateAdminRoutes/PrivateAdminRoutes';
 import PrivateRoutes from './components/PrivateRoutes/PrivateRoutes';
-import { Route, Routes } from 'react-router-dom';
-import {
-	privateAdminRoutes,
-	privateRoutes,
-	publicRoutes,
-} from '../../router/routes';
 
 const AppRouter: FC = () => {
 	const { isAuth, role } = useTypedSelector(selectUser);
 	return (
-		<Routes>
-			{isAuth
-				? [...privateAdminRoutes, ...privateRoutes].map((route) => {
-						return (
-							<Route
-								path={route.path}
-								element={route.component}
-								key={route.path}
-							/>
-						);
-				  })
-				: publicRoutes.map((route) => {
-						return (
-							<Route
-								path={route.path}
-								element={route.component}
-								key={route.path}
-							/>
-						);
-				  })}
-		</Routes>
+		<>
+			{isAuth && role === UserRoles.ADMIN ? (
+				<PrivateAdminRoutes />
+			) : isAuth ? (
+				<PrivateRoutes />
+			) : (
+				<PublicRoutes />
+			)}
+		</>
 	);
 };
 
