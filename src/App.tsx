dfdef8ea76f9504a7c0dd135bf4c3440a706ec85
Vitalii Borshchov <vitalii.v.borshchov@gmail.com>
2023-05-15 14:11:55 +0300

    fix: removed unused selectors from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import { LOCAL_STORAGE } from './constants';
 import { BrowserRouter } from 'react-router-dom';
-import { useTypedSelector } from './hooks/useTypedSelector';
 import { useActions } from './hooks/useActions';
 import AppRouter from './components/AppRouter/AppRouter';
-import { selectUser } from './store/servisces';
 
 function App() {
 	const { loginUser } = useActions();
-	const { isAuth, role, token } = useTypedSelector(selectUser);
 
 	useEffect(() => {
 		const token = localStorage.getItem(LOCAL_STORAGE.TOKEN);
